482db74cb85bdfc73b4f2c7caae711d71908799a
Tommaso Massaglia <tommaso.massaglia@alkemy.com>
2025-06-18 14:44:15 +0200

    added a simple test for all the sklearn pipeline.

--- examples/ml/3_gridsearch_example.py
+++ examples/ml/3_gridsearch_example.py
@@ -5,7 +5,10 @@ from sklearn.model_selection import train_test_split
 from sklearn.svm import LinearSVC
 
 from astrodata.ml.metrics.SklearnMetric import SklearnMetric
-from astrodata.ml.model_selection.GridSearchSelector import GridSearchCVSelector
+from astrodata.ml.model_selection.GridSearchSelector import (
+    GridSearchCVSelector,
+    GridSearchSelector,
+)
 from astrodata.ml.models.SklearnModel import SklearnModel
 
 data = load_breast_cancer()
@@ -30,13 +33,26 @@ gss = GridSearchCVSelector(
     metrics=None,
 )
 
+# gss = GridSearchSelector(
+#    model,
+#    param_grid={
+#        "C": [0.1, 1, 10],
+#        "max_iter": [1000, 2000],
+#        "tol": [1e-3, 1e-4],
+#    },
+#    scorer=accuracy,
+#    val_size=0.2,
+#    random_state=42,
+#    metrics=None,
+# )
+
 print(gss)
 
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.25, random_state=42
 )
 
-gss.fit(X_train, y_train)
+gss.fit(X_train, y_train, X_test=X_test, y_test=y_test)
 print(gss.get_best_params())
 print(gss.get_best_model())
 print(type(gss.get_best_model()))
--- examples/ml/5_mlflow_tracking.py
+++ examples/ml/5_mlflow_tracking.py
@@ -6,7 +6,10 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 
 from astrodata.ml.metrics.SklearnMetric import SklearnMetric
-from astrodata.ml.model_selection.GridSearchSelector import GridSearchCVSelector
+from astrodata.ml.model_selection.GridSearchSelector import (
+    GridSearchCVSelector,
+    GridSearchSelector,
+)
 from astrodata.ml.models.SklearnModel import SklearnModel
 from astrodata.tracking.MLFlowTracker import SklearnMLflowTracker
 
@@ -49,6 +52,20 @@ gss = GridSearchCVSelector(
     log_all_models=False,
     metrics=metrics,
 )
+
+# gss = GridSearchSelector(
+#    gradientboost,
+#    val_size=0.2,
+#    param_grid={
+#        "n_estimators": [50, 100],
+#        "learning_rate": [0.01, 0.1],
+#        "max_depth": [3, 5],
+#    },
+#    scorer=logloss,
+#    tracker=tracker,
+#    log_all_models=False,
+#    metrics=metrics,
+# )
 
 gss.fit(X_train, y_train, X_test=X_test, y_test=y_test)
 
